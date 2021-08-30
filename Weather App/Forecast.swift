a0442e9039da4b9edda364371de30fd13e9d3e4b
fbara <fbara1@yahoo.com>
2021-08-30 13:52:04 -0500

    Start of video 8: Forecast ViewModel.

---
+++ Weather App/Forecast.swift
@@ -0,0 +1,35 @@
+//
+//  Forecast.swift
+//  Forecast
+//
+//  Created by Frank Bara on 8/30/21.
+//
+
+import Foundation
+
+struct Forecast:Codable {
+    struct Daily: Codable {
+        let dt: Date
+        struct Temp: Codable {
+            let min: Double
+            let max: Double
+        }
+        let temp: Temp
+        let humidity: Int
+        struct Weather: Codable {
+            let id: Int
+            let description: String
+            let icon: String
+            var weatherIconURL: URL {
+                let urlString = "http://openweathermap.org/img/wn/\(icon)@2x.png"
+                return URL(string: urlString)!
+            }
+        }
+        let weather: [Weather]
+        let clouds: Int
+        let pop: Double
+    }
+    let daily: [Daily]
+    
+    
+}
